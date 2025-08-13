7b7affd99a23604255ee5c1569a354d485a2a467
EIEIZA555 <s6503051634039@email.kmutnb.ac.th>
2025-08-13 14:15:58 +0700

    RefsArray
    RefsArray

--- src/App.js
+++ src/App.js
@@ -2,12 +2,9 @@ import React from 'react'
 import Button from './class-components';
 import {Calculator2} from './calculator';
 import { EventData1 } from './event-data';
+import RefsArray from './refs-array';
 
 
-function App() {
-  
-  return <EventData1/>
-
+export default function App() {
+  return <RefsArray />
 }
-
-export default App;

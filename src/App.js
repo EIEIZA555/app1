dac1c751b562684578f16abf1b8d5b4a8d65c64c
EIEIZA555 <s6503051634039@email.kmutnb.ac.th>
2025-08-13 14:38:14 +0700

    useStatefunc
    useStatefunc

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import Button from './class-components';
 import {Calculator2} from './calculator';
 import { EventData1 } from './event-data';
 import RefsArray from './refs-array';
+import MessageBox from './state-func';
 
 
 export default function App() {
-  return <RefsArray />
+  return <MessageBox/>
 }

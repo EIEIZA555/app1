9c50ace92a48346ff783da654f6b2f1deac75613
EIEIZA555 <s6503051634039@email.kmutnb.ac.th>
2025-09-08 10:06:07 +0700

    Navlink
    Navlink

--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
-import React, { createContext, useState } from 'react'
+import React from 'react'
+import Router2 from './router2'
 
-import Menu from './component/Menu';
-import Quiz from './component/Quiz';
-import Score from './component/Score';
-import './App.css'
 
-export const DataContext = createContext()
-
-function App() {
-  const [appState, setAppState] = useState("menu")
-  const [score,setScore] = useState(0)
-  return (
-    <DataContext.Provider value={{appState,setAppState,score,setScore}}>
-    <div className="App">
-      <h1>Web Developement Quiz</h1>
-      {appState === "menu" && <Menu/>} 
-      {appState === "quiz" && <Quiz/>} 
-      {appState === "score" && <Score/>} 
-    </div>
-    </DataContext.Provider>
-  );
+export default function App(){
+  return <Router2/>
 }
-export default App;
